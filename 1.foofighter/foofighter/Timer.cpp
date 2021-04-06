f635b46be6fc6116027573510c523f6afee94c7b
Dawid Litwicki <dawidli@stud.ntnu.no>
2021-04-06 21:31:38 +0200

    Added Timer class, that Simen made

---
+++ 1.foofighter/foofighter/Timer.cpp
@@ -0,0 +1,18 @@
+#include "Timer.h"
+
+Timer::Timer()
+{
+  nextTimeout = 0;
+}
+
+
+void Timer::getTimer(int duration)
+{
+  nextTimeout = millis() + duration;
+}
+
+bool Timer::timerHasExpired()
+{
+  bool timerExpired = (millis() >= nextTimeout);
+  return timerExpired;
+}
