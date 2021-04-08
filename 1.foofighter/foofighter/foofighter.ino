c74d2e025e890d6f440433e56dd333722e363163
Dawid Litwicki <dawidli@stud.ntnu.no>
2021-04-08 17:29:54 +0200

    Cleaned up the code

--- 1.foofighter/foofighter/foofighter.ino
+++ 1.foofighter/foofighter/foofighter.ino
@@ -14,34 +14,36 @@ Movement mov;
 Timer rev_timer;
 Timer turn_timer;
 
+//Name of Public Library
+//-------------------------
+ZumoBuzzer buzzer;
+ZumoMotors flip;
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
+
 //=======================================================================
+//Constants and Variables
 
 const int LED = 13;
-
-const int l_detect = 1000;
+const int L_detect = 1000;
 const int rev = 1001;
 const int turn = 1002;
-int state = l_detect;
+int state = L_detect;
+const int NUM_SENSORS = 6;
+unsigned int sensor_values[NUM_SENSORS];
 
 // this might need to be tuned for different lighting conditions, surfaces, etc.
 const int QTR_THRESHOLD = 1500; // microseconds
 
 // these might need to be tuned for different motor types
-
 const int REVERSE_SPEED = 400; // 0 is stopped, 400 is full speed
 const int TURN_SPEED = 400;
 const int FORWARD_SPEED = 200;
 const int REVERSE_DURATION = 1000; // ms
 const int TURN_DURATION = 2000; // ms
 
-ZumoBuzzer buzzer;
-ZumoMotors flip;
-Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
-
-const int NUM_SENSORS = 6;
-unsigned int sensor_values[NUM_SENSORS];
-
-ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
+//========================================================================
+// Functions (might remove later, not sure what class to include them in)
 
 void waitForButtonAndCountDown()
 {
@@ -51,99 +53,100 @@ void waitForButtonAndCountDown()
 
   // play audible countdown
   for (int i = 0; i < 3; i++)
-  {
+    {
     delay(1000);
     buzzer.playNote(NOTE_G(3), 200, 15);
-  }
+    }
   delay(1000);
   buzzer.playNote(NOTE_G(4), 500, 15);
   delay(1000);
 }
 
 void changeStateTo (int newState)
-{
+  {
   state = newState;
-}
+  }
+
+void sensorValues()
+  {
+  Serial.print("Sensor 0 detects: ");
+  Serial.println(sensor_values[0]);
+  Serial.print("Sensor 5 detects: ");
+  Serial.println(sensor_values[5]);
+  Serial.println("");  
+  }
+
+
+//========================================================================
 
 void setup()
 {
- //uncomment if necessary to correct motor directions
+  Serial.begin(9600);
   flip.flipLeftMotor(false);
   flip.flipRightMotor(false);
-  Serial.begin(9600);
-
   pinMode(LED, HIGH);
-
+  
   waitForButtonAndCountDown();
 }
 
+
+
 void loop()
-{
+  {
 
   // Initialize speeds, and puts them inside of Movement class
   mov.initSpeed(FORWARD_SPEED, REVERSE_SPEED, TURN_SPEED, REVERSE_DURATION, TURN_DURATION);
   
   // Iitial start, ( gjør om til funksjon)
   if (button.isPressed())
-  {
+    {
     // if button is pressed, stop and wait for another press to go again
     mov.wait();
     button.waitForRelease();
     waitForButtonAndCountDown();
-  }
-  
+    }
+
+  // Checking what values sensors are detecting, printing them to Serialmonitor,
+  // so that we can adjust QTR_THRESHHOLD for the enviorment 
   sensors.read(sensor_values); 
-/*
-  Serial.print("Sensor 0 detects: ");
-  Serial.println(sensor_values[0]);
-  Serial.print("Sensor 5 detects: ");
-  Serial.println(sensor_values[5]);
-  Serial.println("");
-*/
-//Serial.println(state);
+  // sensorValues();
 
-switch(state)
-{
-  case l_detect:
-  //Serial.println("state1");
-    if (sensor_values[0] > QTR_THRESHOLD)                  // if leftmost sensor detects line, start reversing sequence
-    {
-    // Start two timers, which control the action inside rev_n_turn_R
-    rev_timer.getTimer(REVERSE_DURATION);
-    changeStateTo(rev);
-    }
-    else if (sensor_values[5] > QTR_THRESHOLD)                  // if rightmost sensor detects line, start reversing sequence
-    {
-    rev_timer.getTimer(REVERSE_DURATION);
-    changeStateTo(rev);
-    }
 
-    else
-    {
-      mov.forward();                            //if nothing is detected move straight
-    }
 
+
+  switch(state)
+    {
+    case L_detect:
+      if ((sensor_values[0] > QTR_THRESHOLD) or (sensor_values[5] > QTR_THRESHOLD))            // if any of the sensors detects a line, go to rev case and start timer
+        {
+        rev_timer.getTimer(REVERSE_DURATION);
+        changeStateTo(rev);
+        }
+  
+      else
+        {
+        mov.forward();                                  //if nothing is detected move straight
+        }
+  
     break;
     
-  case rev:                                    //if the ground sensor detected something, start reversing
-  //Serial.println("state2");
-    mov.rev();
-    if (rev_timer.timerHasExpired())
-    {
-      turn_timer.getTimer(TURN_DURATION);
-      changeStateTo(turn);
-    }
+    case rev:                                           //if the ground sensor detected something, start reversing
+      mov.rev();
+      if (rev_timer.timerHasExpired())
+        {
+        turn_timer.getTimer(TURN_DURATION);
+        changeStateTo(turn);
+        }
     break;
 
-   case turn:                                 //when the reversing is done, start turning
-   //Serial.println("state3");
-     mov.turn_R();
-     if (turn_timer.timerHasExpired())
-    {
-      changeStateTo(l_detect);
-    }
+    case turn:                                          //when the reversing is done, start turning
+      mov.turn_R();
+      if (turn_timer.timerHasExpired())
+        {
+        changeStateTo(L_detect);
+        }
     break;
     
-}
-}
+    }
+  }
   
