26dbbfb62ba1f354806bdc36766f2c0f3221b3b4
DESKTOP-LTB042G\zaime <simen.hansen.skaara@gmail.com>
2021-04-18 18:45:41 +0200

    cleaned up in the main code

--- 1.foofighter/foofighter/foofighter.ino
+++ 1.foofighter/foofighter/foofighter.ino
@@ -41,22 +41,27 @@ const int forward_R = 1007;
 const int search = 1008;
 int currentState = search;
 
+//=======================================================================
+// array for the ground sensor
 
 const int NUM_SENSORS = 6;
 unsigned int sensor_values[NUM_SENSORS];
 
-const int IR_Left = 6;
-const int IR_Right = A2;
+//=======================================================================
+// constant variables to the IR-sensors
+
+//const int IR_Left = 6;
+//const int IR_Right = A2;
 const int IR_L_SENS_PIN = A0;
 const int IR_R_SENS_PIN = A1;
 const int IRLimit = 300;
 
+//=======================================================================
 // the treshold for the floor sensors
-// this might need to be tuned for different lighting conditions, surfaces, etc.
+
 const int QTR_THRESHOLD = 1500; // microseconds
 
 //motor speed
-// these might need to be tuned for different motor types
 const int REVERSE_SPEED = 200; // 0 is stopped, 400 is full speed
 const int TURN_SPEED = 200;
 const int FORWARD_SPEED = 200;
@@ -111,8 +116,8 @@ void setup()
   flip.flipLeftMotor(true);
   flip.flipRightMotor(false);
   pinMode(LED, HIGH);
-  pinMode(IR_Left, OUTPUT);
-  pinMode(IR_Right, OUTPUT);
+  //pinMode(IR_Left, OUTPUT);
+  //pinMode(IR_Right, OUTPUT);
 
   waitForButtonAndCountDown();
  
@@ -129,8 +134,9 @@ void loop()
   if(test_timer.timerHasExpired())
     {
     mov.wait();  
-    }*/
- /* if (button.isPressed())
+    }
+    */
+  if (button.isPressed())
     {
     // if button is pressed, stop and wait for another press to go again
     mov.wait();
@@ -142,107 +148,103 @@ void loop()
   // so that we can adjust QTR_THRESHHOLD for the enviorment 
   sensors.read(sensor_values); 
   // sensorValues();
-*/
-//================================================================================
 //Actual actions after initializing
+//================================================================================
+//IR-readings
+int ir_R = IR_R.readIR(IR_R_SENS_PIN, IRLimit);
+int ir_L = IR_L.readIR(IR_L_SENS_PIN, IRLimit);
+//digitalWrite(IR_Left, HIGH);
+//digitalWrite(IR_Right, HIGH);
 
-  int ir_R = IR_R.readIR(IR_R_SENS_PIN, IRLimit); // Infra red sensor reading
-  int ir_L = IR_L.readIR(IR_L_SENS_PIN, IRLimit);
-  digitalWrite(IR_Left, HIGH);
-  digitalWrite(IR_Right, HIGH);
+float A_0 = analogRead(IR_L_SENS_PIN);
+float A_1 = analogRead(IR_R_SENS_PIN);
+Serial.print("A0 sens value e ");
+Serial.println(A_0);
+Serial.print("A1 sens value e ");
+Serial.println(A_1);
+//delay(500);
 
-  float penis = analogRead(IR_L_SENS_PIN);
-  float rav = analogRead(IR_R_SENS_PIN);
-  Serial.print("venstre sens value e ");
-  Serial.println(rav);
-  Serial.print("høyre sens value e ");
-  Serial.println(rav);
-  delay(500);
-  
-//Serial.println(ir);
 //================================================================================
 // movement controll
-  if(sensor_values[5] > QTR_THRESHOLD) 
-    {
-    rev_timer.getTimer(REVERSE_DURATION);  
-    changeStateTo(rev_R);
-    }
-  else if(sensor_values[0] > QTR_THRESHOLD)
-    {
-    rev_timer.getTimer(REVERSE_DURATION);  
-    changeStateTo(rev_L); 
-    }
-  
-  else if (currentState > 1002 and ir_L and ir_R)
+if(sensor_values[5] > QTR_THRESHOLD) 
+  {
+  rev_timer.getTimer(REVERSE_DURATION);  
+  changeStateTo(rev_L);
+  }
+else if(sensor_values[0] > QTR_THRESHOLD)
+  {
+  rev_timer.getTimer(REVERSE_DURATION);  
+  changeStateTo(rev_R); 
+  }
+else if (currentState > 1002 and ir_L and ir_R)
   {
     changeStateTo(forward);
-    } 
-     else if (currentState > 1002 and ir_L and !ir_R)
+  } 
+else if (currentState > 1002 and ir_L and !ir_R)
   {
     changeStateTo(forward_L);
-    }  
-     else if (currentState > 1002 and !ir_L and ir_R)
+  }  
+else if (currentState > 1002 and !ir_L and ir_R)
   {
     changeStateTo(forward_R);
-    }   
-  else if (currentState > 1004 and !ir_L and !ir_R)
-    {
+  }   
+else if (currentState > 1004 and !ir_L and !ir_R)
+  {
     changeStateTo(search);  
-    }
+  }
+  
 //================================================================================
 // motor controll
- /*
-  switch(currentState)
-    {
-    case forward:
-      mov.forward();
-    break;
 
-    case forward_L:
+switch(currentState)
+  {
+  case forward:
+    mov.forward();
+  break;
+
+  case forward_L:
     mov.forward_L();
-    break;
+  break;
 
-    case forward_R:
+  case forward_R:
     mov.forward_R();
-    break;
+  break;
 
-    case rev_L:
-      mov.rev();
-      if(rev_timer.timerHasExpired())
-        {
-        turn_timer.getTimer(TURN_DURATION);
-        changeStateTo(turn_R); 
-        }
-    break;
+  case rev_L:
+    mov.rev();
+    if(rev_timer.timerHasExpired())
+      {
+      turn_timer.getTimer(TURN_DURATION);
+      changeStateTo(turn_L); 
+      }
+  break;
 
-    case rev_R:
-      mov.rev();
-      if(rev_timer.timerHasExpired())
-        {
-        turn_timer.getTimer(TURN_DURATION);
-        changeStateTo(turn_L);  
-        }
-    break;
+  case rev_R:
+    mov.rev();
+    if(rev_timer.timerHasExpired())
+      {
+      turn_timer.getTimer(TURN_DURATION);
+      changeStateTo(turn_R);  
+      }
+  break;
 
-    case turn_R:
-      mov.turn_R();
-      if(turn_timer.timerHasExpired())
-        {
-        changeStateTo(search);
-          
-        }
-    break;
+  case turn_R:
+    mov.turn_R();
+    if(turn_timer.timerHasExpired())
+      {
+      changeStateTo(search);          
+      }
+  break;
 
-    case turn_L:
-      mov.turn_L();
-      if(turn_timer.timerHasExpired())
-        {
-        changeStateTo(search);  
-        
-        }
-        break;
+  case turn_L:
+    mov.turn_L();
+    if(turn_timer.timerHasExpired())
+      {
+      changeStateTo(search);     
+      }
+  break;
 
-     case search:
-     mov.wait();
-     break;
-    }*/}
+  case search:
+  mov.wait();
+  break;
+}}
